d15db7602a2dc329bf09fb19b65707252c3aed34
leonardopollina <leonardo.pollina@epfl.ch>
2019-03-29 11:40:38 +0100

    Create main.m

---
+++ Code/main.m
@@ -0,0 +1,8 @@
+% AML-Project 2 - Courtine part
+%
+% Group Members : - Giovanna Aiello
+%                 - Gaia Carparelli 
+%                 - Marion Claudet
+%                 - Martina Morea 
+%                 - Leonardo Pollina
+
