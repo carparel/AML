130f0913aed32a84c5a1de691dfdfe79be1f54f3
Martina Morea <marty9701@hotmail.it>
2019-04-03 12:57:26 +0200

    Structural reorganization fo filtered data

--- Code/create_Kin_struct_filtered.m
+++ Code/create_Kin_struct_filtered.m
@@ -1,4 +1,4 @@
-function [struct_Kin_filtered] = create_Kin_struct_filtered(Kin_struct,Fs,type)
+function [ Healthy_subjects, SCI_subjects] = create_Kin_struct_filtered(Healthy_subjects,SCI_subjects,subject,Fs)
 % BLABLABLABLA
 % 
 %
@@ -7,18 +7,24 @@ trials = {'T_01', 'T_02', 'T_03'};
 markers_SCI = {'LASI','RASI','LKNE','RKNE','LTOE','RTOE','LANK','RANK'};
 markers_H = {'LHIP','RHIP','LKNE','RKNE','LTOE','RTOE','LANK','RANK'};
 
+SCI_Kin = create_Kin_struct(SCI_subjects,'SCI');
+Healthy_Kin = create_Kin_struct(Healthy_subjects.(subject),'Healthy');
+
 for condition = 1:length(conditions)
     for trial = 1:length(trials)
-        temporary_struct = Kin_struct.(conditions{condition}).(trials{trial});
-        if strcmp(type,'SCI')
+        temporary_struct = SCI_Kin.(conditions{condition}).(trials{trial});
             for marker = 1:length(markers_SCI)
-                struct_Kin_filtered.(conditions{condition}).(trials{trial}).(markers_SCI{marker}) = filtering_Kin(temporary_struct.(markers_SCI{marker}),Fs);
+                 Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Filtered.Kin.(markers_SCI{marker}) = filtering_Kin(temporary_struct.(markers_SCI{marker}),Fs);
             end
-        elseif strcmp(type,'Healthy')
+    end
+end
+
+for condition = 1:length(conditions)
+     for trial = 1:length(trials)
+         temporary_struct = Healthy_Kin.(conditions{condition}).(trials{trial});
             for marker = 1:length(markers_H)
-                struct_Kin_filtered.(conditions{condition}).(trials{trial}).(markers_H{marker}) = filtering_Kin(temporary_struct.(markers_H{marker}),Fs);
+                SCI_subjects.(conditions{condition}).(trials{trial}).Filtered.Kin.(markers_H{marker}) = filtering_Kin(temporary_struct.(markers_H{marker}),Fs);
             end
-        end
     end
 end
 
