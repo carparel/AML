c049a7c08c4b4911340273fde5a2dfb745a3e904
Martina Morea <marty9701@hotmail.it>
2019-04-04 18:17:49 +0200

    Fixed the detection point

--- Code/detect_gait_events_healthy.m
+++ Code/detect_gait_events_healthy.m
@@ -11,13 +11,9 @@ function [Healthy_subjects] = detect_gait_events_healthy(Healthy_subjects,subjec
 
 datas = Healthy_subjects.(subject);
 
-
-legs = {'Right','Left'};
 conditions = {'NO_FLOAT', 'FLOAT'};
 trials = {'T_01', 'T_02', 'T_03'};
 markers = {'Heel','Toe'};
-markers_names = {'LANK','LTOE'};
-markers_names = {'RANK','RTOE'};
 
 % We initialize the events and the threshold
 HS=[];
@@ -27,11 +23,12 @@ TO=[];
 
 threshold=[];
 
+leg = 'Right';
+markers_names = {'RANK','RTOE'};
+
 for condition = 1:length(conditions)
     for trial = 1:length(trials)
-        for marker = 1:length(markers)
-            for leg = 1:length(legs)
-                
+        for marker = 1:length(markers)                
                 
                   coefficient_dilation_marker = length(datas.(conditions{condition}).(trials{trial}).Raw.Kin.LTOE)/ ...
             length(datas.(conditions{condition}).(trials{trial}).Filtered.Kin.LTOE);
@@ -39,13 +36,7 @@ for condition = 1:length(conditions)
         
         coefficient_dilation_emg = length(datas.(conditions{condition}).(trials{trial}).Raw.EMG.RMG)/ ...
             length(datas.(conditions{condition}).(trials{trial}).Filtered.EMG.envelope.RMG);
-                
-                if strcmp(legs{leg},'left')
-                    markers_names = {'LANK','LTOE'};
-                elseif strcmp(legs{leg},'right')
-                    markers_names = {'RANK','RTOE'};
-                end
-                
+                                
                 if strcmp(markers{marker},'Heel') %If we consider the Heel, we take into account the ANKLE marker
                     marker_position_to_consider = datas.(conditions{condition}).(trials{trial}).Filtered.Kin.(markers_names{1});
                     signal = marker_position_to_consider(:,2); % Just Y-Pos
@@ -56,11 +47,11 @@ for condition = 1:length(conditions)
                         threshold =4;
                         [HS,HO] = plateau_endpoints(signal,threshold);
                     end
-                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker = HS; % We put the events in the structure
-                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HO_marker = HO;
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HS_marker = HS; % We put the events in the structure
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HO_marker = HO;
                     
-                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg = (HS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
-                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HO_emg = (HO*coefficient_dilation_marker)/coefficient_dilation_emg;
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HS_emg = (HS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HO_emg = (HO*coefficient_dilation_marker)/coefficient_dilation_emg;
                 end
                 
             if strcmp(markers{marker},'Toe') %If we consider the Toe, we take into account the TOE marker
@@ -73,15 +64,65 @@ for condition = 1:length(conditions)
                     threshold =4;
                     [TS,TO] = plateau_endpoints(signal,threshold);
                 end
-                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TS_marker = TS;
-                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_marker = TO;
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TS_marker = TS;
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TO_marker = TO;
                 
-                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TS_emg = (TS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
-                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_emg = (TO*coefficient_dilation_marker)/coefficient_dilation_emg;
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TS_emg = (TS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TO_emg = (TO*coefficient_dilation_marker)/coefficient_dilation_emg;
+            end
+            end
+        end
+end
+    
+leg = 'Left';
+markers_names = {'LANK','LTOE'};
+
+for condition = 1:length(conditions)
+    for trial = 1:length(trials)
+        for marker = 1:length(markers)                
+                
+                  coefficient_dilation_marker = length(datas.(conditions{condition}).(trials{trial}).Raw.Kin.LTOE)/ ...
+            length(datas.(conditions{condition}).(trials{trial}).Filtered.Kin.LTOE);
+        
+        
+        coefficient_dilation_emg = length(datas.(conditions{condition}).(trials{trial}).Raw.EMG.RMG)/ ...
+            length(datas.(conditions{condition}).(trials{trial}).Filtered.EMG.envelope.RMG);
+                
+                if strcmp(markers{marker},'Heel') %If we consider the Heel, we take into account the ANKLE marker
+                    marker_position_to_consider = datas.(conditions{condition}).(trials{trial}).Filtered.Kin.(markers_names{1});
+                    signal = marker_position_to_consider(:,2); % Just Y-Pos
+                    if strcmp(conditions{condition},'FLOAT')
+                        threshold = 12; % We have empirically found that the threshold for the FLOAT is 8, for the NO_FLOAT is 4
+                        [HS,HO] = plateau_endpoints(signal,threshold); % We determine Heel Strike and Heel Off
+                    elseif strcmp(conditions{condition},'NO_FLOAT')
+                        threshold = 8;
+                        [HS,HO] = plateau_endpoints(signal,threshold);
+                    end
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HS_marker = HS; % We put the events in the structure
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HO_marker = HO;
+                    
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HS_emg = (HS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).HO_emg = (HO*coefficient_dilation_marker)/coefficient_dilation_emg;
+                end
+                
+            if strcmp(markers{marker},'Toe') %If we consider the Toe, we take into account the TOE marker
+                marker_position_to_consider = datas.(conditions{condition}).(trials{trial}).Filtered.Kin.(markers_names{2});
+                signal = marker_position_to_consider(:,2);
+                if strcmp(conditions{condition},'FLOAT')
+                    threshold = 12;
+                    [TS,TO] = plateau_endpoints(signal,threshold);
+                elseif strcmp(conditions{condition},'NO_FLOAT')
+                    threshold = 8;
+                    [TS,TO] = plateau_endpoints(signal,threshold);
+                end
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TS_marker = TS;
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TO_marker = TO;
+                
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TS_emg = (TS*coefficient_dilation_marker)/coefficient_dilation_emg; % We put the events in the structure
+                Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Event.(leg).TO_emg = (TO*coefficient_dilation_marker)/coefficient_dilation_emg;
             end
             end
         end
     end
     
-end
 end
--- Code/plot_gait_events.m
+++ Code/plot_gait_events.m
@@ -1,4 +1,4 @@
-function [] = plot_gait_events(Healthy_subjects,subject,condition,trial,marker)
+function [] = plot_gait_events(Healthy_subjects,subject,condition,trial,marker,leg)
 
 % This function plots the gait events together with the signal for ankle
 % and toe only.
@@ -9,10 +9,10 @@ figure;
 plot(signal(:,2));
 hold on;
     if (strcmp(marker,'LTOE') || strcmp(marker,'RTOE'))
-        scatter(events.TS,signal(events.TS,2));
-        scatter(events.TO,signal(events.TO,2));
+        scatter(events.(leg).TS_marker,signal(events.(leg).TS_marker,2));
+        scatter(events.(leg).TO_marker,signal(events.(leg).TO_marker,2));
     else
-        scatter(events.HS,signal(events.HS,2));
-        scatter(events.HO,signal(events.HO,2));
+        scatter(events.(leg).HS_marker,signal(events.(leg).HS_marker,2));
+        scatter(events.(leg).HO_marker,signal(events.(leg).HO_marker,2));
     end
 end
