da087a5ad7ea003a24afa33fb5f1be7ded22ae2d
Martina Morea <marty9701@hotmail.it>
2019-04-05 15:57:47 +0200

    Remotion of artefacts
    Sorry forgot to open my branch :((

--- Code/normalize.m
+++ Code/normalize.m
@@ -1,6 +1,13 @@
 function [normalized_EMG] = normalize(Data)
 
-% This function normalizes the signal with its maximum
+% This function removes artifacts and normalizes the signal with its maximum
+
+Th = max(Data)/10;
+for i=1:length(Data)
+    if (abs(Data(i))<Th)
+        Data(i)=0;
+    end
+end
 
 normalized_EMG = Data/max(Data);
 
