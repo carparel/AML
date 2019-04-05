57d1a1382fa20edbd9b3645879777df8496bb8a7
leonardopollina <leonardo.pollina@epfl.ch>
2019-04-05 18:50:12 +0200

    let's walk

--- Code/append_gait_events.m
+++ Code/append_gait_events.m
@@ -1,4 +1,4 @@
-function [Healthy_subjects]= append_gait_events(Healthy_subjects)
+function [Healthy_subjects]= append_gait_events(Healthy_subjects,fs_KIN,fs_EMG)
 % This function appends to the original structure all the gait events.
 
 % Subject 1 and 2 are not included for the moment because they are not
@@ -8,6 +8,6 @@ number_sub = length(fieldnames(Healthy_subjects));
 index_subject = [3,4,5,6];
     for i = 1:length(index_subject)
     subject = ['S_' num2str(index_subject(i))];
-    [Healthy_subjects] = detect_gait_events_healthy(Healthy_subjects,subject);
+    [Healthy_subjects] = detect_gait_events_healthy(Healthy_subjects,subject,fs_KIN,fs_EMG);
     end
 end
--- Code/split_into_gaits_healthy.m
+++ Code/split_into_gaits_healthy.m
@@ -13,21 +13,21 @@ envelopes = {'envelope','noenvelope'};
 for condition = 1:length(conditions)
     for trial = 1:length(trials)
         for leg = 1:length(legs)
-
+            
             
             if strcmp(legs{leg},'Right')
-            markers = {'RASI','RKNE','RTOE','RANK'};
-            emgs = {'RMG','RTA'};
+                markers = {'RASI','RKNE','RTOE','RANK'};
+                emgs = {'RMG','RTA'};
             elseif strcmp(legs{leg},'Left')
-            markers = {'LASI','LKNE','LTOE','LANK'};
-            emgs = {'LMG','LTA'};
+                markers = {'LASI','LKNE','LTOE','LANK'};
+                emgs = {'LMG','LTA'};
             end
             
             
             for nb_steps = 1:length(datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker)-1
                 for marker = 1:length(markers)
                     old_signal = datas.(conditions{condition}).(trials{trial}).Filtered.Kin.(markers{marker});
-                     Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).Kin.(markers{marker}) = ...
+                    Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).Kin.(markers{marker}) = ...
                         old_signal(datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker(nb_steps) : ...
                         datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker(nb_steps+1));
                 end
@@ -35,23 +35,23 @@ for condition = 1:length(conditions)
                 
                 
                 for emg = 1:length(emgs)
-%                     
+                    %
                     for envelope = 1:length(envelopes)
                         if strcmp(envelopes{envelope},'envelope')
                             old_signal = datas.(conditions{condition}).(trials{trial}).Filtered.EMG.envelope.(emgs{emg});
-                             Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).EMG.envelope.(emgs{emg}) = ...
+                            Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).EMG.envelope.(emgs{emg}) = ...
                                 old_signal((datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg(nb_steps)) : ...
                                 (datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg(nb_steps+1)));
                         elseif strcmp(envelopes{envelope},'noenvelope')
                             old_signal = datas.(conditions{condition}).(trials{trial}).Filtered.EMG.noenvelope.(emgs{emg});
-                             Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).EMG.noenvelope.(emgs{emg}) = ...
+                            Healthy_subjects.(subject).(conditions{condition}).(trials{trial}).Parsed{nb_steps}.(legs{leg}).EMG.noenvelope.(emgs{emg}) = ...
                                 old_signal((datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg(nb_steps)) : ...
                                 (datas.(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg(nb_steps+1)));
+                        end
                     end
                 end
             end
         end
     end
-end
 end
 end
