13f99b28cd9ef6ae41ac0c6e998924a1413279f8
leonardopollina <leonardo.pollina@epfl.ch>
2019-04-11 19:48:20 +0200

    nothing important

--- Code/find_clusters.m
+++ Code/find_clusters.m
@@ -47,52 +47,6 @@ ylabel('PC_2','FontSize',15);
 zlabel('PC_3','FontSize',15);
 title('Distribution of our data once projected on the first 3 PCs','FontSize',15);
 
-%% K-MEANS and Miss-clustered points
-
-% Here, we apply kmeans imposing a number of cluster equal to 4 
-% --> SCI/Healthy and FLOAT/NO FLOAT in all combinations
-
-% We try to compare the clusters coming from the algorithm k-means with the
-% true ones and we plot the miss-clustered points (onto the plot already
-% existing)
-
-nbr_clusters = 4;
-idx_clusters = kmeans(PCA_data,nbr_clusters);
-
-idx_1 = (idx_clusters == 1);
-idx_2 = (idx_clusters == 2);
-idx_3 = (idx_clusters == 3);
-idx_4 = (idx_clusters == 4);
-
-idx_assigned_k_mean_1 = mode(idx_clusters(ground_truth_1));
-missclustered_1 = find(idx_clusters(ground_truth_1) ~= idx_assigned_k_mean_1);
-current_PCA_data = PCA_data(ground_truth_1,:,:);
-for i = 1:length(missclustered_1)
-    plot(current_PCA_data(missclustered_1(i),1),current_PCA_data(missclustered_1(i),2),current_PCA_data(missclustered_1(i)),3,'y^','MarkerSize',8,'MarkerFaceColor','y');
-end
-
-idx_assigned_k_mean_2 = mode(idx_clusters(ground_truth_2));
-missclustered_2 = find(idx_clusters(ground_truth_2) ~= idx_assigned_k_mean_2);
-current_PCA_data = PCA_data(ground_truth_2,:,:);
-for i = 1:length(missclustered_2)
-    plot3(current_PCA_data(missclustered_2(i),1),current_PCA_data(missclustered_2(i),2),current_PCA_data(missclustered_2(i),3),'y^','MarkerSize',8,'MarkerFaceColor','y');
-end
-
-idx_assigned_k_mean_3 = mode(idx_clusters(ground_truth_3));
-missclustered_3 = find(idx_clusters(ground_truth_3) ~= idx_assigned_k_mean_3);
-current_PCA_data = PCA_data(ground_truth_3,:,:);
-for i = 1:length(missclustered_3)
-    plot3(current_PCA_data(missclustered_3(i),1),current_PCA_data(missclustered_3(i),2),current_PCA_data(missclustered_3(i),3),'y^','MarkerSize',8,'MarkerFaceColor','y');
-end
-
-idx_assigned_k_mean_4 = mode(idx_clusters(ground_truth_4));
-missclustered_4 = find(idx_clusters(ground_truth_4) ~= idx_assigned_k_mean_4);
-current_PCA_data = PCA_data(ground_truth_4,:,:);
-for i = 1:length(missclustered_4)
-    plot3(current_PCA_data(missclustered_4(i),1),current_PCA_data(missclustered_4(i),2),current_PCA_data(missclustered_4(i),3),'y^','MarkerSize',8,'MarkerFaceColor','y');
-end
-
-
 set(h,'Position',[0 0 680 480]);
 end
 
