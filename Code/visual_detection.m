098feb00f83f82bb3ec663b985dc711483d7a827
leonardopollina <leonardo.pollina@epfl.ch>
2019-04-12 19:14:26 +0200

    Visual detection on Z
    Sooooo, the visual detection of the events performed on the small hills (the Z coordinate of the signal of each marker) is done. I stocked everything in a structure containing all the events... maybe it would be nice to create a function to transfer for each trial these events in the original structure (ad it was for find_events() ) or adapt the already existing function to split the fait by make it take into account this ground_truth structure.
    I added in the main few lines about the visual detection --> OF COURSE THEY ARE COMMENTED SINCE WE DO NOT WANT TO REPEAT THIS HELL EVERY TIME. However, in the same subsection we load the existing structure with the ground_truth events previously detected.
    There is also a function called chek_ground_truth_events(), which plot the stair (Y coordinate) together with the hill (Z coordinate) and the detected points to visualize how well it worked... By looking at the hills it seems really nice and good, but it's true that sometimes on the stair it does not correspond at all to what we found...so maybe it can really improve something? Let's hope :)
    
    Nothing to add,
    have fun,
    kiss kiss
    Gossip Boy

---
+++ Code/visual_detection.m
@@ -0,0 +1,77 @@
+function [struct_events] = visual_detection(Healthy_2018,Healthy_2019)
+
+years = {'2018','2019'};
+conditions = {'FLOAT','NO_FLOAT'};
+trials = {'T_01','T_02','T_03'};
+legs = {'Right','Left'};
+
+% for plot titles (in order to not have the boring underscores)
+types = {'FLOAT', 'NO FLOAT'};
+trials_title = {'Trial 1','Trial 2','Trial 3'};
+
+for year = 1:length(years)
+    
+    if strcmp(years{year},'2018')
+        data = Healthy_2018;
+        subjects = 4;
+    else
+        data = Healthy_2019;
+        subjects = [1,2,3];
+    end
+    
+    for subject = 1:length(subjects)
+        current_subject = data.(['S_' num2str(subjects(subject))]);
+        for condition = 1:length(conditions)
+            for trial = 1:length(trials)
+                current_trial = current_subject.(conditions{condition}).(trials{trial}).Filtered.Kin;
+                for leg = 1:length(legs)
+                    
+                    if strcmp(legs{leg},'Right')
+                        markers = {'RTOE','RANK'};
+                    else
+                        markers = {'LTOE','LANK'};
+                    end
+                    
+                    for marker = 1:length(markers)
+                        current_marker = current_trial.(markers{marker});
+                        Z_coordinate = current_marker(:,3);
+                        
+                        % The first time is to find the strike events.
+                        figure();
+                        hold on;
+                        grid on;
+                        plot(Z_coordinate,'r-','LineWidth',1.2);
+                        XMIN = 0;
+                        XMAX = length(Z_coordinate);
+                        YMIN = 0;
+                        YMAX = 1000;
+                        axis([XMIN XMAX YMIN YMAX]);
+                        title({[years{year} '; ' ['S_' num2str(subjects(subject))] ' ;'  types{condition} ' ; ' trials_title{trial} '; Leg = ' legs{leg} '; Marker =' markers{marker}],'Find the strike points'});
+                        % Press Return key to exit.
+                        [struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).STRIKE,~] = ginput();
+                        struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).STRIKE = round(struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).STRIKE);
+                        
+                        % The second time is to find the off events.
+                        figure();
+                        hold on;
+                        grid on;
+                        plot(Z_coordinate,'r-','LineWidth',1.2);
+                        XMIN = 0;
+                        XMAX = length(Z_coordinate);
+                        YMIN = 0;
+                        YMAX = 1000;
+                        axis([XMIN XMAX YMIN YMAX]);
+                        title({[years{year} '; ' ['S_' num2str(subjects(subject))] ' ;' types{condition} ' ; ' trials_title{trial} '; Leg = ' legs{leg} '; Marker =' markers{marker}],'Find the OFF points'});
+                        % Press Return key to exit.
+                        [struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).OFF,~] = ginput();
+                        struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).OFF = round(struct_events.(['Year_' years{year}]).(['S_' num2str(subjects(subject))]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).(markers{marker}).OFF);
+                    end
+                end
+            end
+        end
+    end
+    
+% To save the events and to not have to repeat this procedure at every run!
+save('Events.mat','struct_events');
+end
+
