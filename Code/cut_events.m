ff2f55cd332e28f9339d0abe82f461cc6829aaf5
Giovanna Aiello <giovannaiello28@gmail.com>
2019-04-15 11:53:11 +0200

    Little fix

--- Code/cut_events.m
+++ Code/cut_events.m
@@ -40,10 +40,14 @@ for s = nbr_subjects(1):nbr_subjects(end)
                 
                 heel_strikes = Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker;
                 toe_offs = Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_marker;
+                heel_strikes_emg = Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg;
+                toe_offs_emg = Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_marker;
                 
                 if heel_strikes(1) > toe_offs(1)
                     Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_marker = heel_strikes(1:length(toe_offs)-1);
+                    Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).HS_emg = heel_strikes_emg(1:length(toe_offs)-1);
                     Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_marker = toe_offs(2:end);
+                    Healthy_struct.(['S_' num2str(s)]).(conditions{condition}).(trials{trial}).Event.(legs{leg}).TO_emg = toe_offs_emg(2:end);
                 end
                 
             end
